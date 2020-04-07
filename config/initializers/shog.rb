407a135bf7946a8e0757d7ce454836c36f043af5
l.jackson96 <l.jackson96@outlook.com>
2020-04-07 16:04:11 -0700

    Done.

---
+++ config/initializers/shog.rb
@@ -0,0 +1,10 @@
+Shog.configure do
+    if ::Rails.env.production?
+      reset_config!
+      timestamp
+    end
+    match /execution expired/ do |msg,matches|
+      # Highlight timeout errors
+      msg.red
+    end
+  end
